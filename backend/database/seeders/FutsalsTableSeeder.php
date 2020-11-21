33b11d7b937c5a984e04c792243ccd250885baeb
rey0811 <reotogashi@gmail.com>
2020-11-21 18:43:25 +0900

    create futsals table

---
+++ backend/database/seeders/FutsalsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class FutsalsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('futsals')->insert([
+            [
+            'name' => 'futsal 1',
+            'place' => 'tokyo',
+            'url' => 'xxx',
+            'created_at' => new Datetime(),
+            'updated_at' => new Datetime()
+        ],
+            [
+            'name' => 'futsal 2',
+            'place' => 'tokyo',
+            'url' => 'yyy',
+            'created_at' => new Datetime(),
+            'updated_at' => new Datetime()
+        ],
+        ]);
+    }
+}
